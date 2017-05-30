9ad675b18d06e0c37c4849d1693d881270c5413a
Ayberk Piyade <ayberkpiyade@gmail.com>
2017-05-30 12:40:25 -0700

    logo uploAD

--- SaleStore/Areas/Admin/Controllers/CompaniesController.cs
+++ SaleStore/Areas/Admin/Controllers/CompaniesController.cs
@@ -1,12 +1,14 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using SaleStore.Data;
 using SaleStore.Models;
+using Microsoft.AspNetCore.Http;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
 
 namespace SaleStore.Areas.Admin.Controllers
 {
@@ -14,10 +16,12 @@ namespace SaleStore.Areas.Admin.Controllers
     public class CompaniesController : Controller
     {
         private readonly ApplicationDbContext _context;
+        private IHostingEnvironment env;
 
-        public CompaniesController(ApplicationDbContext context)
+        public CompaniesController(IHostingEnvironment _env,ApplicationDbContext context)
         {
-            _context = context;    
+            _context = context;
+            this.env = _env;
         }
 
         // GET: Admin/Companies
@@ -55,13 +59,56 @@ namespace SaleStore.Areas.Admin.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Name,Address,Phone,Logo,Id,CreateDate,CreatedBy,UpdatedBy,UpdateDate")] Company company)
+        public async Task<IActionResult> Create(Company company, IFormFile uploadFile)
         {
-            if (ModelState.IsValid)
+            if (uploadFile != null && ".jpg,.jpeg,.png".Contains(Path.GetExtension(uploadFile.FileName)) == false)
             {
-                _context.Add(company);
-                await _context.SaveChangesAsync();
-                return RedirectToAction("Index");
+                ModelState.AddModelError("ImageUpload", "Dosyanın uzantısı .jpg, .gif ya da .png olmalıdır.");
+            }
+
+            else if (ModelState.IsValid)
+            {
+                if (uploadFile != null)
+                {
+
+                    company.CreatedBy = User.Identity.Name ?? "username";
+                    company.CreateDate = DateTime.Now;
+                    company.UpdatedBy = User.Identity.Name ?? "username";
+                    company.UpdateDate = DateTime.Now;
+
+                    if (Path.GetExtension(uploadFile.FileName) == ".jpg"
+                    || Path.GetExtension(uploadFile.FileName) == ".gif"
+                    || Path.GetExtension(uploadFile.FileName) == ".png")
+                    {
+                        string category = DateTime.Now.Month + "-" + DateTime.Now.Year;
+                        string FilePath = env.WebRootPath + "\\uploads\\" + category + "\\";
+                        string dosyaismi = Path.GetFileName(uploadFile.FileName);
+                        var yuklemeYeri = Path.Combine(FilePath, dosyaismi);
+                        company.Logo = "uploads/" + category + "/" + dosyaismi;
+                        try
+                        {
+                            if (!Directory.Exists(FilePath))
+                            {
+                                Directory.CreateDirectory(FilePath);//Eðer klasör yoksa oluþtur    
+                            }
+                            using (var stream = new FileStream(yuklemeYeri, FileMode.Create))
+                            {
+                                await uploadFile.CopyToAsync(stream);
+                            }
+
+
+                            _context.Add(company);
+                            await _context.SaveChangesAsync();
+                            return RedirectToAction("Index");
+                        }
+                        catch (Exception exc) { ModelState.AddModelError("Logo", "Hata: " + exc.Message); }
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("WrongType", "Dosya uzantýsý izin verilen uzantýlardan olmalýdýr.");
+                    }
+                }
+                else { ModelState.AddModelError("FileExist", "Lütfen bir dosya seçiniz!"); }
             }
             return View(company);
         }
