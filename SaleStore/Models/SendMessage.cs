505dffecd30d9b54b3cfb09ad79252c36f947d73
OnurKAPICI <onurkapici@gmail.com>
2017-06-07 15:41:18 +0300

    MailSettingInboxSendMessageModelAndControllerAdded

---
+++ SaleStore/Models/SendMessage.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SaleStore.Models
+{
+    public class SendMessage:BaseEntity
+    {
+
+        [Required]
+        [StringLength(200)]
+        [Display(Name = "Mailin Konusu")]
+        public string Subject { get; set; }
+        [Required]
+        [StringLength(200)]
+        [Display(Name = "Mailin İçeriği")]
+        public string BodyContent { get; set; }
+        [Display(Name="Mail Ayarı")]
+        public int MailSettingId { get; set; }
+        [ForeignKey("MailSettingId")]
+        public MailSetting MailSetting { get; set; }
+
+    }
+}
