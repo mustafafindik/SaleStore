0c3b42dd45d141e9a1397bd211b0be9a24226c16
Mustafa Fındık <mustafa.findik3@gmail.com>
2017-06-15 23:11:13 +0300

    MyCompanies add

---
+++ SaleStore/Controllers/MyCompaniesController.cs
@@ -0,0 +1,160 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SaleStore.Data;
+using SaleStore.Models;
+
+namespace SaleStore.Controllers
+{
+    public class MyCompaniesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public MyCompaniesController(ApplicationDbContext context)
+        {
+            _context = context;    
+        }
+
+        // GET: MyCompanies
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Companies.Include(c => c.ApplicationUser);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: MyCompanies/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var company = await _context.Companies
+                .Include(c => c.ApplicationUser)
+                .SingleOrDefaultAsync(m => m.Id == id);
+            if (company == null)
+            {
+                return NotFound();
+            }
+
+            return View(company);
+        }
+
+        // GET: MyCompanies/Create
+        public IActionResult Create()
+        {
+            ViewData["UserId"] = new SelectList(_context.ApplicationUser, "Id", "Id");
+            return View();
+        }
+
+        // POST: MyCompanies/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Name,Address,Phone,Logo,ProductCount,CampaignCount,UserId,Id,CreateDate,CreatedBy,UpdatedBy,UpdateDate")] Company company)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(company);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            ViewData["UserId"] = new SelectList(_context.ApplicationUser, "Id", "Id", company.UserId);
+            return View(company);
+        }
+
+        // GET: MyCompanies/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var company = await _context.Companies.SingleOrDefaultAsync(m => m.Id == id);
+            if (company == null)
+            {
+                return NotFound();
+            }
+            ViewData["UserId"] = new SelectList(_context.ApplicationUser, "Id", "Id", company.UserId);
+            return View(company);
+        }
+
+        // POST: MyCompanies/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Name,Address,Phone,Logo,ProductCount,CampaignCount,UserId,Id,CreateDate,CreatedBy,UpdatedBy,UpdateDate")] Company company)
+        {
+            if (id != company.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(company);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CompanyExists(company.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction("Index");
+            }
+            ViewData["UserId"] = new SelectList(_context.ApplicationUser, "Id", "Id", company.UserId);
+            return View(company);
+        }
+
+        // GET: MyCompanies/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var company = await _context.Companies
+                .Include(c => c.ApplicationUser)
+                .SingleOrDefaultAsync(m => m.Id == id);
+            if (company == null)
+            {
+                return NotFound();
+            }
+
+            return View(company);
+        }
+
+        // POST: MyCompanies/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var company = await _context.Companies.SingleOrDefaultAsync(m => m.Id == id);
+            _context.Companies.Remove(company);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        private bool CompanyExists(int id)
+        {
+            return _context.Companies.Any(e => e.Id == id);
+        }
+    }
+}
